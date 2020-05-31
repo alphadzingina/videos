1e856caaf0005bd8eb97ec3ab27da97fa8e17837
alphadzingina <alphaadx6@gmail.com>
2020-05-31 12:05:34 +0100

    add some styling to video detail

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,7 +5,12 @@ const VideoDetail = (props) => {
         return <div>Loading...</div>;
     }
     return(
-        <div>{props.video.snippet.title}</div>
+        <div>
+            <div className="ui segment">
+                <h4 className="ui header">{props.video.snippet.title}</h4>
+                <p>{props.video.snippet.description}</p>
+            </div>
+        </div>
     );
 };
 
